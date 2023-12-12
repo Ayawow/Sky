c24fa8940a1bea564cbfb9f2c700fd2e947c6190
Ayawow <1123325454@qq.com>
2023-12-12 15:02:42 +0800

    公共字段自动填充

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -88,13 +88,13 @@ public class EmployeeServiceImpl implements EmployeeService {
         employee.setPassword(DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes()));
 
         //设置当前记录时间和修改时间
-        employee.setCreateTime(LocalDateTime.now());
-        employee.setUpdateTime(LocalDateTime.now());
+//        employee.setCreateTime(LocalDateTime.now());
+//        employee.setUpdateTime(LocalDateTime.now());
 
         //设置当前人id和修改人id
 //        TODO 改为当前登录用户的id
-        employee.setCreateUser(BaseContext.getCurrentId());
-        employee.setUpdateUser(BaseContext.getCurrentId());
+//        employee.setCreateUser(BaseContext.getCurrentId());
+//        employee.setUpdateUser(BaseContext.getCurrentId());
 
         employeeMapper.insert(employee);
     }
@@ -150,8 +150,8 @@ public class EmployeeServiceImpl implements EmployeeService {
        Employee employee =  new Employee();
        BeanUtils.copyProperties(employeeDTO,employee);
 
-       employee.setUpdateTime(LocalDateTime.now());
-       employee.setUpdateUser(BaseContext.getCurrentId());
+//       employee.setUpdateTime(LocalDateTime.now());
+//       employee.setUpdateUser(BaseContext.getCurrentId());
 
        employeeMapper.update(employee);
     }
